1c9aeef1d9b58f0684c2e9ec9419f9ae32d29969
2246375441 <2246375441@qq.com>
2020-07-21 16:18:51 +0800

    supermall

---
+++ src/network/request.js
@@ -0,0 +1,38 @@
+import axios from 'axios'
+
+export function request(config){
+  // 1.创建axios的实例
+  const instance = axios.create({
+    baseURL:'http://123.207.32.32:8000',
+    timeout:10000
+  })
+
+
+  // 2.axios的拦截器
+  // 请求拦截
+  instance.interceptors.request.use(config => {
+    return config
+  },err => {
+  })
+
+  // 响应拦截
+  instance.interceptors.response.use(res => {
+    return res
+  },err => {
+  })
+
+  // 3.发送真正的网络请求
+  // instance(config)
+  //   .then(res => {
+  //     // console.log(res)
+  //     success(res)
+  //   })
+  //   .catch(err => {
+  //     // console.log(err)
+  //     failure(err)
+  //   })
+
+    return instance(config)
+    
+
+} 
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -10,12 +10,31 @@
 
 <script>
 import NavBar from 'components/common/navbar/NavBar'
+import {getHomeMultidata} from 'network/home'
 
 export default {
   name:'Home',
   components:{
     NavBar
-  }
+  },
+  data() {
+    return {
+      result:null,
+      banners:[], 
+      recommends:[],
+    }
+  },
+  // 声明周期函数  首页创建完成之后 直接触发
+  created() {
+    // 1 请求多个数据
+    getHomeMultidata().then(res => {
+      // 将端口数据存入 data中的result中
+      this.result = res
+
+      this.banners = res.data.data.banner.list;
+      this.recommends = res.data.data.recommend.list;
+    })
+  },
 }
 </script>
 
