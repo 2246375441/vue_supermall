fd27de237c734a8eaa689748dc981ba7c4317605
2246375441 <2246375441@qq.com>
2020-08-07 19:46:42 +0800

    supermall

--- src/views/cart/childComps/CartBottonBar.vue
+++ src/views/cart/childComps/CartBottonBar.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="bottom-bar">
     <div class="check-content">
-      <check-button class="check-button"></check-button>
+      <check-button class="check-button" :is-checked="isSelectAll" @click.native="checkCilck"></check-button>
       <span>全选</span>
     </div>
 
@@ -17,6 +17,7 @@
 
 <script>
 import CheckButton from '../../../components/content/checkButton/CheckButton'
+import { mapGetters } from 'vuex'   //可以用... 代替 vuex中的getters.js 映射成局部计算属性
 export default {
   name:'CartBottonBar',
   components:{
@@ -34,6 +35,36 @@ export default {
     },
     checkLength(){
       return this.$store.state.cartList.filter(item => item.checked).length
+    },
+    isSelectAll(){
+      // 原点 选中 和 未选中 状态
+      // 0取反为true  >0 取反为false
+      // 先从整个数组中 筛选有没有没被选中的 ，有的话 length长度会加1 
+
+      if(this.$store.state.cartList.length === 0) return false
+
+      // 1 使用filter
+      // return !(this.$store.state.cartList.filter(item => !item.checked).length)
+
+      // 2 使用find
+      return !this.$store.state.cartList.find(item => !item.checked)
+
+      // 3 使用便利for
+      // for(let item of this.$store.state.cartList){
+      //   if(!item.checked){
+      //     return false
+      //   }
+      // }
+      // return true
+    }
+  },
+  methods: {
+    checkCilck(){
+      if(this.isSelectAll){
+        this.$store.state.cartList.forEach(item => item.checked = false)
+      }else{
+        this.$store.state.cartList.forEach(item => item.checked = true)
+      }
     }
   },
 }
