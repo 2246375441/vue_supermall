5e5a2efcd38b9959e6ef6c4576b5af6f7b0036b9
陈先生 <2246375441@qq.com>
2020-11-01 13:30:39 +0800

    supermall

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,8 @@
+const path = require('path');//引入path模块
+function resolve(dir) {
+  return path.join(__dirname, dir)//path.join(__dirname)设置绝对路径
+}
+
 module.exports = {
   configureWebpack:{
     resolve:{
